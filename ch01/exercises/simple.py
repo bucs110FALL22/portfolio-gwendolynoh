9f91df5fdb6fc0a4287a4092289548fa1e23b49a
Gwendolyn Oh <112440162+gwendolynoh@users.noreply.github.com>
2022-09-06 21:56:47 +0000

    I completed it

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,18 @@
+print (10*5)
+print (10**2)
+print (15/10)
+print (15//10)
+print (-15//10)
+print (15%10)
+print (10%15)
+print (0%10)
+print (10/15)
+# the number end but it is suppose to be an infinte number 
+
+print ("what is the exchange rate for the Euro to Dollar")
+rate = int(input())
+print ("what is the amount of currency to exchange")
+amount = int(input())
+total = amount*rate 
+result = total - 3 
+print (result)
