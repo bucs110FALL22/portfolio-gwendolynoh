e2d2a806385b11f93caf599d83a0d43c9f4695cd
Gwendolyn Oh <112440162+gwendolynoh@users.noreply.github.com>
2022-11-05 00:19:22 +0000

    Ch07/lab main.py, rectangle.py, surface.py

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,12 +1,20 @@
 class Rectangle:
-  def __init__(self,x,y,height,width):
-    self.x = x
-    self.y = y   
-    self.height = height
-    self.width = width
+  def __init__(self, x, y, h, w):
+    '''
+Function sets the instance variables x,y, height, and width of the rectangle so that the values cannot be lower than 0. 
 
-  def __str__(self):
-    result = f" x is {self.x}, y is {self.y}, height is {self.height}, width is {self.width}"
-    return(result)
-  
+    '''
+    self.x = max(0,x)
+    self.y = max(0,y) 
+    self.height = max(0,h)
+    self.width = max(0,w)
     
+  def __str__(self):
+    '''
+The function uses f-strings to return strings of x,y,height, and width but does not print the values 
+    '''
+    str_result = f" x:{self.x}, y:{self.y}"
+    str_result += f" \nheight:{self.height}"
+    str_result += f" \nwidth:{self.width}"
+    return str_result
+  
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,9 +1,15 @@
 from Rectangle import Rectangle 
 
 class Surface:
-  def __init__(self,filename,x, y, height, width):
+  def __init__(self,filename,x, y, h, w):
+    '''
+Function takes the parameter x,y,height, and width and creates and object that is then stored in self.rect. Filename is stored in the instance variable self.image
+    '''
     self.image = filename
-    self.rect = Rectangle(x,y,height,width)
+    self.rect = Rectangle(x, y, h, w)
     
   def getRect(self):
-    return(self.rect)
+    '''
+    Function returns retangle object 
+    '''
+    return self.rect
