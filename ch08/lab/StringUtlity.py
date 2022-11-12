a76acda2607efbda530bd11ccdb734fe251da167
Gwendolyn Oh <112440162+gwendolynoh@users.noreply.github.com>
2022-11-12 04:13:54 +0000

    Ch08/lab

---
+++ ch08/lab/StringUtlity.py
@@ -0,0 +1,38 @@
+
+
+class StringUtility:
+  def __init__(self,string):
+    self.string = string
+    return string 
+
+
+  def __str__(self):
+    return string
+
+  def vowels(self):
+    self.vowels = ['a','e','i','o','u']
+    self.count = 0 
+    for char in string:
+      if char in vowels:
+        count += 1
+        if count >= 5:
+          count = print("many")
+          return count 
+    print(count)
+
+  def bothEnds(self):
+    if len(str) < 2:
+      return ''
+    return str[0:2] + str[-2:]
+
+  def fixStart(self):
+    return str[0] + str[1].replace(str[0],'*')
+
+  def asciiSum(self):
+    sum(ord(string)
+
+  def cipher(self):
+     
+    
+
+    
